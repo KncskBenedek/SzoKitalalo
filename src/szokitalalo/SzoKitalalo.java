ab759fa7efbeeea932e52beb960bf340084c6112
KncskBenedek <koncsikbenedek@gmail.com>
2022-03-21 15:31:03 +0100

    kiindulás, dokumentáció

---
+++ src/szokitalalo/SzoKitalalo.java
@@ -0,0 +1,30 @@
+package szokitalalo;
+/*
+leírás saját szavainkal: A program kiválaszt egy kétbetűs szót több szó közül
+(pl.: íz, őz fű), ezután a felhasználótol bekérünk egy maximum 2 karakter hosszú 
+szót amit ??? módon a kiválasztott szóhoz hasonlítjuk, ha egy betű bennevan és
+rossz helyen van jelezzük hogy benne van csak rossz helyen.
+Ha kitalálta ???.
+
+
+*randomSzó();
+*bekér();
+*karakterreBontás();
+*karakterEllenőrzés(); -->
+|->benneVanE();  joHelyenVanE();
+*kiiras();
+*torzs();
+
+
+
+*/
+
+public class SzoKitalalo {
+
+    public static void main(String[] args) {
+        torzs();
+    }
+    private static void torzs(){
+    
+    }
+}
