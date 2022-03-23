5e31a177e6c70a4803e48ead99a6d1f45579c7c9
KncskBenedek <koncsikbenedek@gmail.com>
2022-03-23 22:19:50 +0100

    karakter ellenőrzés kezd

--- src/szokitalalo/SzoKitalalo.java
+++ src/szokitalalo/SzoKitalalo.java
@@ -20,11 +20,42 @@ Ha kitalálta ???.
 */
 
 public class SzoKitalalo {
-
+    static String rndSzo = "";
+    static String beSzo = "";
+    static char[] 
     public static void main(String[] args) {
         torzs();
     }
     private static void torzs(){
-    
+        randomSzo();
+        karakterEllenorzes();
+    }
+    private static void karakterEllenorzes(tomb){
+        int i  = 0;
+        //int dbNemJo = 0;
+        int hossz = tomb.lenght;
+        while (i<hossz) {
+            
+            boolean benneVan = benneVanE(tomb[i]);
+            
+            //if(benneVan){
+                //boolean joHelyenVan = jóhelyenvan
+                //kiir()
+                //String szoveg = joHelyenVan?"":"";
+                //kiir(szoveg);            
+            //}else{
+                //dbNemJo++;
+            //}
+            
+              //boolean ar =   joHelyenVanE();  
+            //i++;
+            
+            //System.out.printf(ar?"":"");
+        }
+    }
+
+    private static benneVanE(char betu){
+        
+        
     }
 }
