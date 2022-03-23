115f05197d48f74f403136570474b094ee2cd3df
Norbus2000 <odornorbert.on@gmail.com>
2022-03-23 22:20:01 +0100

    bekeresKesz

--- src/szokitalalo/SzoKitalalo.java
+++ src/szokitalalo/SzoKitalalo.java
@@ -1,4 +1,12 @@
 package szokitalalo;
+
+
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+
+
 /*
 leírás saját szavainkal: A program kiválaszt egy kétbetűs szót több szó közül
 (pl.: íz, őz fű), ezután a felhasználótol bekérünk egy maximum 2 karakter hosszú 
@@ -21,10 +29,29 @@ Ha kitalálta ???.
 
 public class SzoKitalalo {
 
-    public static void main(String[] args) {
-        torzs();
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        //torzs();
+        beker();
     }
     private static void torzs(){
     
     }
+
+    private static String beker() throws UnsupportedEncodingException {
+        Scanner sc = new Scanner(System.in);
+        String szo = "";
+        // /^[A-Za-zÁÉÍÓÖŐÚÜŰáéíóöőúüű]{1,2}$/         /^[a-zA-Z]+$/g
+        do {
+            System.out.println("Kérem adjon meg egy szót: ");
+            Scanner scan = new Scanner(new InputStreamReader(System.in, "ISO-8859-1"));
+            szo=scan.nextLine();
+        } while (!(szo.matches("[a-záéíóöőúüű]{2}")));
+        return szo;
+        // ellenorizni a random szot
+      
+        
+    } 
+    
+    
+    
 }
